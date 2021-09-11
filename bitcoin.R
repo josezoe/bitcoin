6c968862a618af1eb3607d1e54509c1a2a5b279f
josezoe <66567383+josezoe@users.noreply.github.com>
2021-09-11 04:56:25 -0400

    Update bitcoin.R

--- bitcoin.R
+++ bitcoin.R
@@ -1,4 +1,4 @@
-********************************************************
+*******************************************************
   # The Goal of this Kernel is to analyze the bitcoin close price 
   # predict a 14 day forecast 
   
@@ -44,8 +44,61 @@ df=data
 
 
 # ***********************************
-# stripping time 1325317920  to date format 
+# stripping time 1325317920  to
+# date format  by giving origin date and time to format we need
 
 # ************************************
 
 df$date<- strptime(as.POSIXct(df$Timestamp,origin="1970-01-01 00:00.00"),"%Y-%m-%d")
+
+
+  # ***********************************
+  # convert date as factor 
+  # ************************************
+
+df$date=as.factor(df$date)
+# ******************************************************
+# Creating new data to focus on Close prices per day
+# **********************************************
+df_close=df %>% 
+  group_by(date) %>% 
+  summarise(Close=mean(Close,na.rm=T))
+
+df_close$date <- as.Date(df_close$date)
+df_close <- as.data.frame(df_close)
+
+row.names(df_close) <- df_close$date
+
+plot(df_close, ylab="Close Price Bitcoin", type="l")
+
+
+# removing days for future prediction
+forecast_time=14
+df_input <- head(df_close, -forecast_time)
+df_test <- tail(df_close, forecast_time)
+
+# Creating time series for modeling and future comparing of prediction
+
+input_ts <- ts(df_input$Close,start=c(2012,01,01),frequency=365)
+test_ts <- ts(df_test$Close,start=end(input_ts)+c(0,1),frequency=365)
+
+# Modeling Holt-Winter
+input_ts.hw <- HoltWinters(input_ts, seasonal = "add")
+input_ts.hw
+# Modeling auto arimar
+input_ts.arima <- auto.arima(input_ts, seasonal = T,approximation=F,stepwise=F)
+
+input_ts.arima
+
+# Using arima model ARIMA(2,2,3) 
+bitforc=forecast::forecast(input_ts.arima,h=14)
+bitforc
+summary(bitforc)
+bitforchw=forecast::forecast(input_ts.hw,h=14)
+summary(bitforchw)
+
+plot(bitforc)
+plot(bitforchw,start="2021-03-01")
+autoplot(bitforchw,start="2021-03-01")
+
+
